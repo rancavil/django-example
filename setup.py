938f5dcbb279ce327fa7b6c0e90ef6745e3444a0
Rodrigo Ancavil del Pino <rancavil@innovaser.cl>
2013-11-05 22:49:14 -0300

    use of django 1.5.5 and django restframework

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     description='OpenShift App',
     author='Your Name',
     author_email='example@example.com',
-    url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django<=1.4'],
+    url='https://pypi.python.org/pypi',
+    install_requires=['Django<=1.5.5', 'djangorestframework'],
 )
