0351203eb35037f110d35ae6c3c157e1861c2f3d
Rodrigo Ancavil del Pino <rancavil@innovaser.cl>
2013-11-06 00:24:24 -0300

    config ALLOWED_HOST for debug False

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -46,7 +46,10 @@ else:
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+if DEBUG == False:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
